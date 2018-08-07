2a147d559a4a18dfb4cebce0bd514daa40b81fad
孙季 <sunji@playcrab.com>
2018-08-07 17:56:20 +0800

    preface opt

--- preface.go
+++ preface.go
@@ -7,6 +7,12 @@ import (
 	"github.com/fatih/color"
 )
 
+var pf *Preface
+
+func init() {
+	pf = NewPreface()
+}
+
 //Preface 前言结构体
 type Preface struct {
 	KV map[string]interface{}
@@ -14,7 +20,14 @@ type Preface struct {
 
 //NewPreface new instance
 func NewPreface() *Preface {
-	return new(Preface)
+	p := new(Preface)
+	p.KV = make(map[string]interface{})
+	return p
+}
+
+//Reset 充值单例
+func Reset() {
+	pf = NewPreface()
 }
 
 //ShowPreface 展示前言(直接展示一条)
@@ -22,22 +35,22 @@ func ShowPreface(k string, v interface{}) {
 	NewPreface().Set(k, v).Show()
 }
 
+//SetPreface 设置
+func SetPreface(k string, v interface{}) *Preface { return pf.Set(k, v) }
+
 //Set 设置前言
 func (p *Preface) Set(k string, v interface{}) *Preface {
-	//PrefKV Preface值的单例
-	p.KV = make(map[string]interface{})
 	p.KV[k] = v
 	return p
 }
 
-//MultSet 批量设置前言
-func (p *Preface) MultSet(ks, vs []string) *Preface {
-	if len(ks) != len(vs) {
-		Erro("preface参数个数不相等")
-	}
-	p.KV = make(map[string]interface{})
-	for k, v := range vs {
-		p.KV[ks[k]] = v
+//SetMultiPreface 批量设置
+func SetMultiPreface(m map[string]string) *Preface { return pf.SetMulti(m) }
+
+//SetMulti 批量设置前言
+func (p *Preface) SetMulti(m map[string]string) *Preface {
+	for k, v := range m {
+		p.KV[k] = v
 	}
 	return p
 }
@@ -47,7 +60,7 @@ func (p *Preface) Show() *Preface {
 	c := color.New(color.FgHiMagenta)
 	c.Println(strings.Repeat("-", 64))
 	for k, v := range p.KV {
-		c.Println(fmt.Sprintf("%s : \t%s", k, v))
+		c.Println(fmt.Sprintf("%-16v |%-v", k, v))
 	}
 	c.Println(strings.Repeat("-", 64))
 	return p
